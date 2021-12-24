78aef04a8575b5f4203a083419b6707af4eb1a2a
Alex Shepel <iam.alex.shepel@gmail.com>
2021-12-24 11:21:05 +0200

    add reselect optimization for filtered items selector

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,12 +1,13 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 const getItems = state => state.contacts.items;
 const getIsLoading = state => state.contacts.isLoading;
 const getIsAdding = state => state.contacts.isAdding;
 const getDeletingId = state => state.contacts.deletingId;
 const getFilter = state => state.contacts.filter;
-const getFilteredItems = state =>
-  getItems(state).filter(({ name }) =>
-    name.toLowerCase().includes(getFilter(state).toLowerCase()),
-  );
+const getFilteredItems = createSelector(getItems, getFilter, (items, filter) =>
+  items.filter(({ name }) => name.toLowerCase().includes(filter.toLowerCase())),
+);
 
 export {
   getItems,
